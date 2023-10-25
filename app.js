80060b20af4aa7807ff9033e7c46d61b71caf100
BilguunAgii <100501368+BilguunAgii@users.noreply.github.com>
2023-10-25 16:34:06 +0800

    Ecent Listeneruuriig uusgej ogloo

--- app.js
+++ app.js
@@ -1,40 +1,19 @@
-var uiController = (function() {
-    var x = 100;
+var uiController = (function() {})();
   
-    function add(y) {
-      return x + y;
-    }
+var financeController = (function() {})();
   
-    return {
-      publicAdd: function(a) {
-        a = add(a);
-        console.log("Боловсруулсан утга : " + a);
-      }
+var appController = (function(uiController, financeController) {
+    var ctrlAddItem = function(){
+        console.log("Дэлгэцээс өгөгдлөө авах хэсэг");
     };
-  })();
-  
-  var financeController = (function() {})();
-  
-  var appController = (function(uiController, financeController) {
-    uiController.publicAdd(150);
-  })(uiController, financeController);
-  
-  // var hunController = (function() {
-  
-  //     // data encapsulation
-  
-  //     // private data
-  //   var bodol = "Javascript толгой эргүүлмээр юм...";
-  //   function tsusGuih() {}
-  //     // private function
-  //   function huchilturugchiigAgaaraasSorjTsusruuOruulah() {}
-  
-  //   return {
-  //     yarih: function() {
-  //       bodol = "Javascript бол лаг";
-  //       huchilturugchiigAgaaraasSorjTsusruuOruulah();
-  //       tsusGuih();
-  //       console.log("hi");
-  //     }
-  //   };
-  // })();
+
+    document.querySelector(".add__btn").addEventListener("click", function(){
+        ctrlAddItem();
+    });
+
+    document.addEventListener("keypress", function(event){
+        if(event.keyCode === 13 || event.which === 13){
+        ctrlAddItem();
+        }
+    });
+})(uiController, financeController);
